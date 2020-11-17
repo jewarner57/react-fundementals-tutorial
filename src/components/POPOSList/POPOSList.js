9dc6ed767598eeae7e21fbc5e3790afb4eba8034
Jonathan Warner <jewarner57@gmail.com>
2020-11-17 16:17:56 -0500

    added public space search

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,11 +1,20 @@
-import React from 'react';
 import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
 import data from '../../sfpopos-data.json'
+import React, { useState } from 'react'
 
 function POPOSList() {
 
-    const spaces = data.map(({ title, address, images, hours }, i) => {
+    const [query, setQuery] = useState('')
+    const spaces = data.filter((obj) => {
+        // true if query is in title
+        const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+        // true if query is in address
+        const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+        // return true if either is true
+        return inTitle || inAddress
+    }).map((obj, i) => {
+        const { title, address, images, hours } = obj
         return (
             <POPOSSpace
                 id={i}
@@ -19,9 +28,23 @@ function POPOSList() {
     })
 
     return (
-        <div className="POPOSList">
-            { spaces}
+        <div>
+            <div className="POPOSListForm">
+                <form>
+                    <input
+                        value={query}
+                        placeholder="search"
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+            <div className="POPOSList">
+
+                {spaces}
+            </div>
         </div>
+
     )
 }
 
