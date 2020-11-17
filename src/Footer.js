b4142587a5e19267c18ed7931b6e6ab3040150b1
Jonathan Warner <jewarner57@gmail.com>
2020-11-16 19:25:57 -0500

    added footer and content styles

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// src/Footer.js
+import './Footer.css';
+
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="Footer">
+            <p>Jonathan Warner - Copyright 2020</p>
+        </div>
+    )
+}
+
+export default Title
--- src/POPOSSpace.js
+++ src/POPOSSpace.js
@@ -1,10 +1,11 @@
 // src/Project.js
 
 import React from 'react'
+import './POPOSSpace.css'
 
 function POPOSSpace(props) {
     return (
-        <div>
+        <div className="POPOSSpace">
             <img src={`${process.env.PUBLIC_URL}images/${props.image}`} width="300" height="300" alt="Hello" />
             <h1>{props.name}</h1>
             <div>{props.saddress}</div>
