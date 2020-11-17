45891ea215b112c98f8bb86c03a128f69098651a
Jonathan Warner <jewarner57@gmail.com>
2020-11-16 19:04:47 -0500

    Added Page Title/Header

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+// src/Title.js
+import './Title.css';
+
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="Title">
+            <h1>SFPOPOS</h1>
+            <p>San Francisco Privately Owned Public Spaces</p>
+        </div>
+    )
+}
+
+export default Title
