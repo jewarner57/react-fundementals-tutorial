cdb26a07fbea95b74e395a7e5716b9089eb1e86c
Jonathan Warner <jewarner57@gmail.com>
2020-11-17 15:40:54 -0500

    refactored remaining src/components file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import { HashRouter as Router, Route } from 'react-router-dom'
-import Title from './Title/Title';
-import POPOSList from './POPOSList/POPOSList';
-import About from './About/About'
-import POPOSDetails from './POPOSDetails/POPOSDetails'
-import Footer from './Footer';
+import Title from '../Title/Title';
+import POPOSList from '../POPOSList/POPOSList';
+import About from '../About/About'
+import POPOSDetails from '../POPOSDetails/POPOSDetails'
+import Footer from '../Footer/Footer';
 
 function App() {
     return (
--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,7 +1,7 @@
 // src/POPOSList.js
 
 import React from 'react';
-import POPOSSpace from '../POPOSSpace';
+import POPOSSpace from '../POPOSSpace/POPOSSpace';
 import './POPOSList.css';
 import data from '../../sfpopos-data.json'
 
